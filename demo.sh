33cac74bb23a0daad34ed8c1c75564bc782805ab
feng <fengzhen66@outlook.com>
2018-03-10 22:29:48 +0800

    提交

---
+++ demo.sh
@@ -0,0 +1,12 @@
+if [ -d $1 ];then
+	echo "目录已存在！请另起一个!"
+	exit
+else
+	mkdir $1
+	cd $1
+	mkdir css js
+        echo " <!DOCTYPE> <title>Hello</title> <h1>Hi</h1>" > index.html
+	echo "h1{color: red;}" > ./css/style.css
+	echo "var string = "Hello World"；alert(string)" > ./js/main.js
+	exit
+fi
